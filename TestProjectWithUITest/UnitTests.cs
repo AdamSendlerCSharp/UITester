54fda7601714558701d56c9c75c0d07291304196
Marsel Almetov <almetov.mr@finist-soft.ru>
2025-05-26 18:43:44 +0300

    50 параллально

--- TestProjectWithUITest/UnitTests.cs
+++ TestProjectWithUITest/UnitTests.cs
@@ -1,12 +1,12 @@
 namespace TestProjectWithUITest;
 
 [TestFixture]
-//[Parallelizable(ParallelScope.All)]
+[Parallelizable(ParallelScope.All)]
 public class UnitTests: BaseTest
 {
 	private static IEnumerable<Type> GetTestScenarios()
 	{
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < 50; i++)
 		{
 			yield return typeof(Calculator2Minus2Result);
 		}
